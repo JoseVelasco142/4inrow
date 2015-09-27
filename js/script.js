5fc382d4ebf6356b4aa68cff8c25c42adc852094
Jose Velasco <root@Jose-LPC>
2015-09-28 01:37:14 +0200

    funciones++

--- js/script.js
+++ js/script.js
@@ -13,45 +13,39 @@ $( document ).ready(function() {
         context.beginPath();
         context.lineWidth = 7;
         context.shadowColor = 'black';
-        context.strokeStyle = "rgba(0,0,0,0.6)";
-        context.shadowBlur = 15;
-        context.shadowOffsetX = 0;
-        context.shadowOffsetY = 0;
-        context.arc(70 + (177 * c), canvas.height / 2, 60, 0, 2 * Math.PI, false);
+        context.strokeStyle = "rgba(0,0,0,0.8)";
+        context.shadowBlur = 7;
+        context.shadowOffsetX = 1;
+        context.shadowOffsetY = 5;
+        context.arc(75 + (173 * c), canvas.height / 2, 60, 0, 2 * Math.PI, false);
         context.stroke();
         context.restore();
         context.fillStyle = cl;
         context.fill();
         context.strokeStyle = cl;
         context.stroke();
-
     }
 
-    function moveToken(c, f, cl) {
-        drawToken(c, cl);
-
+    function moveToken(f) {
         $("#uicanvas").animate({
-            top: 62 + ((f * 77) + (f + 2))
+            top: 60 + ((f * 77) + (f + 2))
         });
+
     }
 
     function demo(){
-        autodraw(0);
-        autodraw(1);
-        autodraw(2);
-        autodraw(3);
-        autodraw(4);
-    }
+        drawToken(0,'darkred');
+        drawToken(2,'yellow');
+        drawToken(4,'green');
+        moveToken(4);
+        moveToken(1);
+        moveToken(3);
+        moveToken(2);
+        moveToken(0);
+        moveToken(4);
 
-    function autodraw(row) {
-        var c1 = 'red';
-        var c2 = 'yellow';
-        moveToken(0, row, c1);
-        moveToken(1, row, c2);
-        moveToken(2, row, c1);
-        moveToken(3, row, c2);
-        moveToken(4, row, c1);
-        moveToken(5, row, c2);
+
+
     }
 
     $('#iniciar').click(function() {
@@ -97,12 +91,8 @@ $( document ).ready(function() {
 });
 
 
-
-//////////////////////////////////////////////////////////////////////////
-
-
 /*
-   
+
 
     $(canvas).click(function() {
         canvas_clicked = $(this).attr('id');
@@ -120,17 +110,4 @@ $( document ).ready(function() {
             $(this).attr('class', 'has_chip');
             canvas_above.attr('class', 'can_place');
             create_circle(black);
-        }
-    });
-
-    $('undo').click(function(){
-        var radius = 49;
-        create_circle(white, radius);
-    });
-
-    $('reset').click(function(){
-        var radius = 49;
-        for(index = 0; index < canvas.length; index++) {
-            create_circle(white,radius, index);
-        }
-    });*/
+*/
